5db9e9d9769375ca510a95630e43274fce58052f
Yue Ren <yue.ren.kl@gmail.com>
2025-08-01 12:03:58 +0100

    new: second todo

--- helpers.jl
+++ helpers.jl
@@ -4,3 +4,11 @@ using Oscar
 function excise(G,e,n)
     # TODO: create function
 end
+
+function triangle_chain(k)
+   # TODO: implement this function
+end
+
+function triangle_wheel(k)
+    # TODO: implement this function
+end
