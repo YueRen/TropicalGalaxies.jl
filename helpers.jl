4a2a0df061cd762485423f7765697ad5381fd478
Arushi Chauhan <arushi.chauhan030@gmail.com>
2025-08-04 11:34:28 +0100

    new: triangles_chain

--- helpers.jl
+++ helpers.jl
@@ -5,8 +5,37 @@ function excise(G,e,n)
     # TODO: create function
 end
 
-function triangle_chain(k)
-   # TODO: implement this function
+"""
+    triangle_chain(k)
+
+Creates a graph of `k` chained triangles.
+Each triangle shares an edge with the next one.
+
+Returns a SimpleGraph.
+"""
+function triangle_chain(k::Int)
+    # Total number of vertices: 2 shared + 1 new per triangle
+    num_vertices = k + 2
+    g = graph(Undirected, num_vertices)
+
+    # First triangle: vertices 1-2-3
+    add_edge!(g, 1, 2)
+    add_edge!(g, 2, 3)
+    add_edge!(g, 3, 1)
+
+    # Subsequent triangles
+    for i in 1:k-1
+        # Triangle i shares edge (i+1, i+2), add new node (i+3)
+        u = i + 1
+        v = i + 2
+        w = i + 3
+
+        # Safe to add edge since we preallocated num_vertices
+        add_edge!(g, u, w)
+        add_edge!(g, v, w)
+    end
+
+    return g
 end
 
 function triangle_wheel(k)
