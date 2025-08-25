fa7138886ff2647346a1c8d823c4b835f2ef4a87
Arushi Chauhan <arushi.chauhan030@gmail.com>
2025-08-25 19:43:57 +0100

    new: is_forest and laman_graphs

--- tst.jl
+++ tst.jl
@@ -20,3 +20,29 @@ TropG = tropical_linear_space(MG)
 TropGNeg = -TropG
 
 # TropG * (-TropG)
+
+
+# # Tests 
+# g1 = Undirected_MultiGraph(
+#     5,
+#     [(2, 1), (3, 1), (4, 3), (5, 3)]
+# )
+
+# g2 = Undirected_MultiGraph(
+#     7,
+#     [(2, 1), (3, 1), (5, 4), (6, 4), (7, 6)]
+# )
+
+# g3 = Undirected_MultiGraph(
+#     7,
+#     [(2, 1), (3, 1), (5, 4), (6, 4), (7, 6), (7, 5)]
+# )
+
+
+# is_forest(triangle_wheel(4)) # neither
+# is_forest(g1) # tree
+# is_forest(g2) # forest
+# is_forest(g3) # neither
+
+# g = laman_graphs(5)[1]
+# visualize_graph(g)
