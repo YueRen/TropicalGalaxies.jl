45448e40a9f225a936cf25f0c98aa4af88e6167a
Yue Ren <yue.ren.kl@gmail.com>
2025-10-14 17:37:02 +0100

    new: intersection graph of two tropical stars

--- src/export.jl
+++ src/export.jl
@@ -2,6 +2,7 @@ export all_multiedge_excisions
 export arboreal_pair
 export excise
 export graph
+export intersection_graph
 export is_almost_fully_excised
 export is_fully_excised
 export laman_graph
