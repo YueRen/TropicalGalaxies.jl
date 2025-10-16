892e3b1a2613a77b5bcc0c41cd3f4848992c3f43
Yue Ren <yue.ren.kl@gmail.com>
2025-10-16 15:24:31 +0100

    chg: tropical_star.jl cleanup

--- src/tropical_star.jl
+++ src/tropical_star.jl
@@ -37,7 +37,7 @@ Return the tropical linear space of the Tropical Star `HH`.
 """
 function tropical_linear_space(HH::TropicalStar)
     if HH.linearSpace === nothing
-        HH.linearSpace = tropical_linear_space(HH.graph)
+        HH.linearSpace = tropical_linear_space(graph(HH))
     end
     return HH.linearSpace
 end
@@ -49,7 +49,7 @@ end
 Plot the graph of a Tropical Star HH.
 """
 function visualize_graph(HH::TropicalStar)
-    visualize_graph(HH.graph)
+    visualize_graph(graph(HH))
 end
 
 
@@ -71,22 +71,27 @@ TropicalStar(UndirectedMultigraph(5, [(2, 5), (1, 3), (2, 5), (4, 5), (2, 4)]), 
 ```
 """
 function excise(HH::TropicalStar, excisedVertices::Vector{Int})
-    g_excised = excise(HH.graph, excisedVertices) 
-    return tropical_star(g_excised.n_vertices, g_excised.edges)  
+    FFgraph = excise(graph(HH), excisedVertices)
+    return tropical_star(n_vertices(FFgraph), edges(FFgraph))
 end
 
 
 @doc raw"""
-    tropical_intersection_product(HH1::TropicalStar, HH2::TropicalStar)
+    galactic_pairing(HH1::TropicalStar, HH2::TropicalStar)
 
-Return the tropical intersection product of Tropical Stars 'HH1' and 'HH2'.
+Return the galactic pairing of `HH1` and `HH2`, i.e., the tropical intersection product of the tropical linear space of `HH1` and the negated tropical linear space of `HH2`.
 """
-function tropical_intersection_product(HH1::TropicalStar, HH2::TropicalStar)
+function galactic_pairing(HH1::TropicalStar, HH2::TropicalStar)
     # TODO: add basic assertions to check that TropV1 and TropV2 are as expected
     # (of complementary dimension modulo lineality)
-    Trop_HH1 = tropical_linear_space(HH1.graph)
-    Trop_HH2 = tropical_linear_space(HH2.graph)
-    @assert ambient_dim(Trop_HH1) == ambient_dim(Trop_HH2) "different ambient dimensions"
-    return tropical_intersection_number(Trop_HH1, Trop_HH2)
+    TropHH1 = tropical_linear_space(graph(HH1))
+    TropHH2 = tropical_linear_space(graph(HH2))
+    @assert ambient_dim(TropHH1) == ambient_dim(TropHH2) "different ambient dimensions"
+    if n_maximal_polyhedra(TropHH1) > n_maximal_polyhedra(TropHH2)
+        TropHH2 = -TropHH2
+    else
+        TropHH1 = -TropHH1
+    end
+    return tropical_intersection_number(TropHH1, TropHH2)
 end
 
